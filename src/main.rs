30b87016ed970b1bc33acf0c9f81b0d01273fdeb
TilakMaddy <tilak.madichetti@gmail.com>
2023-03-25 20:53:41 +0530

    Duh

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ fn main() -> Result<()> {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
-    let output = std::process::Command::new(command)
+    let mut output = std::process::Command::new(command)
         .args(command_args)
-        .output()
+        .spawn()
         .with_context(|| {
             format!(
                 "Tried to run '{}' with arguments {:?}",
@@ -16,12 +16,7 @@ fn main() -> Result<()> {
             )
         })?;
 
-    if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        print!("{}\n", std_out);
-    } else {
-        std::process::exit(1);
-    }
+    output.wait()?;
 
     Ok(())
 }
